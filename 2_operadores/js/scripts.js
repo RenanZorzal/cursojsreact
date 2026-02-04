97812aee37c2fce8990cec89ded2dc40e6aebf9d
Renan Zorzal <renanzberger@gmail.com>
2026-02-04 17:56:54 -0300

    Curso javascript começo

---
+++ 2_operadores/js/scripts.js
@@ -0,0 +1,16 @@
+//1- Números
+console.log(typeof 1)
+console.log(typeof 3.5)
+console.log(typeof -1)
+
+//2- Operadores
+console.log(1+5)
+console.log(5-2)
+console.log(5*2)
+console.log(10/2)
+
+//3- Special numbers
+console.log(typeof Infinity)
+console.log(typeof -Infinity)
+console.log(typeof NaN)
+console.log(1*"A")
