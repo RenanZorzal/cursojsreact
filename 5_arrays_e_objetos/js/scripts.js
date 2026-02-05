3fc689b7da9fb3e8267acba813b430e3c8962c1d
Renan Zorzal <renanzberger@gmail.com>
2026-02-05 17:51:52 -0300

    arrays e objects

--- 5_arrays_e_objetos/js/scripts.js
+++ 5_arrays_e_objetos/js/scripts.js
@@ -1,18 +1,77 @@
-//1- arrays
-const lista =  [1, 2, 3, 4, 5];
+// //1- arrays
+// const lista =  [1, 2, 3, 4, 5];
 
-console.log(lista);
+// console.log(lista);
 
-console.log(typeof lista);
+// console.log(typeof lista);
 
-const itens = ["Renan", true, 2, 4.12, []];
+// const itens = ["Renan", true, 2, 4.12, []];
 
-console.log(itens);
+// console.log(itens);
 
-//2 - propriedades
-const numbers = [5, 3, 4];
-console.log(numbers.length);
-console.log(numbers["lenght"])
+// //2 - mais sobre arrays
+// const arr = ["a","b","c","d","e"];
+// console.log(arr[0])
 
-const myName = "Renan";
-console.log(myName.length);
+// //3 - propriedades
+// const numbers = [5, 3, 4];
+// console.log(numbers.length);
+// console.log(numbers["lenght"])
+
+// const myName = "Renan";
+// console.log(myName.length);
+
+// //4 - metodos
+// const otherNumbers = [1,2,3];
+
+// const allNumbers = numbers.concat(otherNumbers);
+// console.log(allNumbers);
+// const text = "algum texto";
+// console.log(text.toUpperCase());
+// console.log(typeof text.toUpperCase);
+// console.log(text.indexOf("g"));
+
+// //5 - objetos
+// const person = {
+//     name: "Renan",
+//     age: 20,
+//     job: "Programador",
+// };
+// console.log(person);
+// console.log(person.name);
+// console.log(person.name.length);
+// console.log(typeof person);
+
+//6 - criando e deletando propriedades
+const car ={
+    engine: 2.0,
+    brand: "VW",
+    model: "Tiguan",
+    km: 20000,
+};
+
+console.log(car);
+
+car.doors = 4;
+
+console.log(car);
+
+delete car.km;
+
+console.log(car);
+
+//7 - mais sobre objetos
+const obj = {
+    a: "teste",
+    b: true,
+};
+
+console.log(obj instanceof Object);
+
+const obj2 = {
+    c: [],
+};
+
+Object.assign(obj2,obj);
+
+console.log(obj2);
