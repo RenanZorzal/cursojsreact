c1726af44dd06b716aa9552c8a5e3dd49f80089f
Renan Zorzal <renanzberger@gmail.com>
2026-02-26 16:56:05 -0300

    testes

--- tabuada/js/scripts.js
+++ tabuada/js/scripts.js
@@ -1,4 +1,16 @@
-const enviar = document.querySelector("#enviar");
-enviar.addEventListener("click", ()=>{
-    console.log("Clicou");
+const enviar = document.querySelector("#enviarcampo");
+const conteudo = document.querySelector("#conteudo");
+const campo1 = document.getElementById('campo1');
+const campo2 = document.getElementById('campo2');
+
+
+enviar.addEventListener("click", () =>{
+    
+    for(const x = 0; x < campo2.value; x++){
+        console.log(campo1.value);
+        
+        conteudo.textContent = (x) +"x" +(campo1.value)+"="+ (x*campo1.value);
+    };
+    conteudo.textContent = "Teste";
 })
+
